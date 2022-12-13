6af91d996486ac49fd8f3523a93de1d2c33ac7ec
Nils Hörnström <hornstrom@gmail.com>
2022-12-13 22:46:21 +0100

    Some biler plate for day 13

---
+++ AdventOfCode/Day13/ComDevice.cs
@@ -0,0 +1,36 @@
+﻿namespace AdventOfCode.Day13;
+
+public class ComDevice
+{
+    public ComDevice(string[] data)
+    {
+        for (int i = 0; i < data.Length -1; i++)
+        {
+            
+        }
+    }
+
+    public class PacketPair
+    {
+        public Packet LeftPacket { get; set; }
+        public Packet RightPacket { get; set; }
+
+        public PacketPair(string leftPacket, string rightPacket)
+        {
+            LeftPacket = new Packet(leftPacket);
+            RightPacket = new Packet(rightPacket);
+        }
+    }
+
+    public class Packet
+    {
+        public List<Packet> Packets { get; set; }
+        public int? Actual { get; set; }
+        public Packet(string packet)
+        {
+            
+        }
+    }
+
+    
+}
---
+++ AdventOfCode/Day13/Day13.cs
@@ -0,0 +1,17 @@
+﻿
+
+public class Day13 : Day
+{
+    public string[] Data = System.IO.File.ReadAllLines(@"Day12\data.txt");
+    public string[] DataTest = System.IO.File.ReadAllLines(@"Day12\test_data.txt");
+    
+    public void Part1()
+    {
+        
+    }
+
+    public void Part2()
+    {
+        
+    }
+}
